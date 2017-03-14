15e00268079c03409c90231b5f606d0551562676
mvannortwick <mvannortwick@thermotron.com>
2017-03-14 16:36:54 -0400

    initial commit

---
+++ Backup/route trace to textbox/TextBoxTraceListener.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace route_trace_to_textbox
+{
+  public class TextBoxTraceListener : System.Diagnostics.TraceListener
+  {
+    public delegate void UpdateTextBoxDelegate(string strValue);
+    public event UpdateTextBoxDelegate FireUpdateTextBoxEvent;
+    public override void Write(String message)
+    // You define these and put the message into your textbox.
+    {
+      FireUpdateTextBoxEvent(message);
+    }
+    public override void WriteLine(String message)
+    // You define these and put the message into your textbox.
+    {
+      FireUpdateTextBoxEvent(message + "\r\n");
+    }
+  }
+}
+
---
+++ route trace to textbox/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace route_trace_to_textbox
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+      TextBoxTraceListener textBoxTraceListener = new TextBoxTraceListener();
+      Trace.Listeners.Add(textBoxTraceListener);
+      textBoxTraceListener.FireUpdateTextBoxEvent += new TextBoxTraceListener.UpdateTextBoxDelegate(OnUpdateTextBoxEvent);
+    }
+    void OnUpdateTextBoxEvent(string strValue)
+    {
+      Invoke((MethodInvoker)delegate
+      {
+        textBox1.AppendText(strValue);
+        int nLineCount = textBox1.Lines.GetLength(0);
+        if (nLineCount > 15)
+        {
+          textBox1.Text = textBox1.Text.Remove(0, textBox1.Lines[0].Length + 2);
+        }
+      });
+    }
+    int nCount = 0;
+    private void button1_Click(object sender, EventArgs e)
+    {
+      Trace.WriteLine("Trace Message" + nCount++);
+    }
+  }
+}
